bc29c1bf1b0da5edaf3c6b8d41941890de5a3ffb
fisabilimf <fisabilimaghfirona@gmail.com>
2025-06-12 08:58:44 +0000

    update

--- local/resources/views/layouts/app.blade.php
+++ local/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Dinas Pekerjaan Umum Sumber Daya Air</title>
+    <title>Dinas Pekerjaan Umum Sumber Daya Air - Jawa Timur</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
